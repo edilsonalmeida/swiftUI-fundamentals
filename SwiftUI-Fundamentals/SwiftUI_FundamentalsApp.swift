592696279250eac35844f92788ce7c09c0759932
Edilson Almeida <contato.edilsonalmeida@gmail.com>
2022-06-06 14:18:40 -0300

    Initial Commit

---
+++ SwiftUI-Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI-Fundamentals
+//
+//  Created by edilsonalmeida on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
